17c8f73094395d4342cbb27bac8d0173a8b84582
Mahendran V A <mahe2111@global.chainsys.com>
2019-05-06 14:34:05 +0530

    second commit

--- src/main/java/com/chainsys/auth/Authentication.java
+++ src/main/java/com/chainsys/auth/Authentication.java
@@ -5,9 +5,15 @@ import java.sql.SQLException;
 import com.chainsys.auth.DAO.loginDAO;
 
 public class Authentication {
-	public boolean isLogin(String username,String password) throws ClassNotFoundException, SQLException
+	public static boolean isLogin(String username,String password) throws ClassNotFoundException, SQLException
 	{
-		Boolean value=loginDAO.success(username,password);
-		return value;
+		return loginDAO.success(username,password);
 	}
+//	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//		if(isLogin("mahe@gmail.com", "1234")){
+//			System.out.println("Success");
+//		}else {
+//			System.out.println("failure");
+//		}
+//	}
 }
